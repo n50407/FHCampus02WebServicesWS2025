9f6360f207e2c1ae0232e24c84c9959fefa205e7
Johann Grabner <jgrabner@business-tuning.at>
2025-12-11 10:03:00 +0100

    APIKey - https://www.codeproject.com/articles/Secure-ASP-NET-Core-Web-API-using-API-Key-Authenti#comments-section

---
+++ FHCampus02WebServices2025/Controllers/ApiKeyValuesController.cs
@@ -0,0 +1,18 @@
+﻿using FHCampus02WebServices2025.Attributes;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FHCampus02WebServices2025.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [ApiKey]
+    public class ApiKeyValuesController : ControllerBase
+    {
+        [HttpGet("helloWorldSecure")]
+        public IActionResult Get()
+        {
+            return Ok("You have access to this protected resource.");
+        }
+    }
+}
